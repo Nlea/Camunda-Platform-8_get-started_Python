9720a54ae8ace59c139727e5c725a85e6b865ac5
nlea <nele.uhlemann@posteo.de>
2022-02-10 15:40:37 +0100

    Use asyncio and create an async. function in order to deploy process model

--- client.py
+++ client.py
@@ -7,22 +7,25 @@ from pyzeebe import (
     create_camunda_cloud_channel
 )
 
-load_dotenv()
-my_client_id = os.environ.get('Client_ID')
-my_client_secret = os.environ.get('Client_Secret')
-my_cluster_id = os.environ.get('Cluster_ID')
-
-print(my_cluster_id)
-
-grpc_channel = create_camunda_cloud_channel(
-    client_id= my_client_id,
-    client_secret=my_client_secret,
-    cluster_id= my_cluster_id,
-    region="bru-2",  # Default is bru-2
-)
-
-zeebe_client = ZeebeClient(grpc_channel)
-
-await zeebe_client.deploy_process("greeting-process.bpmn")
 
 
+async def main():
+    load_dotenv()
+    my_client_id = os.environ.get('Client_ID')
+    my_client_secret = os.environ.get('Client_Secret')
+    my_cluster_id = os.environ.get('Cluster_ID')
+    
+    print(my_cluster_id)
+    grpc_channel = create_camunda_cloud_channel(
+        client_id= my_client_id,
+        client_secret=my_client_secret,
+        cluster_id= my_cluster_id,
+        region="bru-2",  # Default is bru-2
+        )
+        
+    zeebe_client = ZeebeClient(grpc_channel)
+    
+    await zeebe_client.deploy_process('greeting-process.bpmn')
+    print('Process deployed')
+
+asyncio.run(main())
