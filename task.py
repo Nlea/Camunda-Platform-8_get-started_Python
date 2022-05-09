a1fa659242f5cb8bc7da85f992f0863affa06cec
Nlea <36015705+Nlea@users.noreply.github.com>
2022-05-09 14:19:30 +0200

    Update task.py

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ from pyzeebe import ZeebeTaskRouter
 router = ZeebeTaskRouter()
 
 
-@router.task(task_type="mail")
+@router.task(task_type="email")
 async def my_task(message_content: str):
     ##Your business logic goes here
     print('Sending email with message content: ' + message_content)
